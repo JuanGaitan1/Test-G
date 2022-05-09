f4efafc6751e009e533c6456f0bc13b71389e312
Juan Gaitan <diablogaitan2@gmail.com>
2022-05-08 23:45:55 -0300

    Finish

--- api/src/database.js
+++ api/src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
+
+const MONGODB_URI = ('mongodb+srv://admin:admin@cluster0.ukho1.mongodb.net/test')
 
 //starting the server
 mongoose.connect (
-    process.env.MONGODB_URI,{})
+    MONGODB_URI,{})
 .then(db=> console.log('DB is connect'))
 .catch(err => console.log(err));
